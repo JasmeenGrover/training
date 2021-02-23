6482dc86bc8303723324b16ff609698874b22fa3
Jameen Grover <jasmeengrover945@gmail.com>
2021-02-23 19:59:47 +0530

    Update registration.php

--- task-2,3/registration.php
+++ task-2,3/registration.php
@@ -121,7 +121,8 @@
   </form>
 </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 
@@ -147,7 +148,7 @@
         var txtPic = $("#txtPic").val();
         var txtTech = $("#txtTech").val();
         var txtHobbies = $("#txtHobbies").val();
-        var url = "registration-process.php?name=" + txtName + "&studentid=" + txtId + "&mobile=" + txtMobile + "&email=" + txtEmail + "&gender"  + txtGender + "&department" + txtDepartment + "&course" + txtCourse + "&pic" + txtPic + "&skills" + txtTech + "&hobbies" +txtHobbies;
+        var url = "registration-process.php?name=" + txtName + "&studentid=" + txtId + "&mobile=" + txtMobile + "&email=" + txtEmail + "&gender="  + txtGender + "&department=" + txtDepartment + "&course=" + txtCourse + "&pic=" + txtPic + "&skills=" + txtTech + "&hobbies=" +txtHobbies;
         alert(url);
         $.get(url, function(response){
           if(response == "Data Saved")
