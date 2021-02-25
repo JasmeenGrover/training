a17366e0458518987f71ff07eb3e0c083845a288
Jameen Grover <jasmeengrover945@gmail.com>
2021-02-26 00:03:20 +0530

    Update registration.php

--- task-2,3/registration.php
+++ task-2,3/registration.php
@@ -20,7 +20,7 @@
         <label for="txtName">Name</label>
       </div>
       <div class="col-lg-5">
-        <input type="text" name="txtName" value="" class="text-field" id="txtName">
+        <input type="text" name="txtName" value="" class="text-field" id="txtName" required>
       </div>
     </div>
     <div class="form-row pt-2">
@@ -69,11 +69,11 @@
         <label for="txtCourse">Select Courses</label>
       </div>
       <div class="col-lg-5">
-        <input type="checkbox" id="txtCourse" name="txtCourse" value="abcd"> abcd <br>
-        <input type="checkbox" id="" name="txtCourse" value="abcde"> abcd <br>
-        <input type="checkbox" id="" name="txtCourse" value="abcd"> abcd <br>
-        <input type="checkbox" id="" name="txtCourse" value="abcd"> abcd <br>
-        <input type="checkbox" id="" name="txtCourse" value="abcd"> abcd
+        <input type="checkbox" id="txtCourse" name="txtCourse[]" value="abcd"> abcd <br>
+        <input type="checkbox" id="" name="txtCourse[]" value="abcde"> abcd <br>
+        <input type="checkbox" id="" name="txtCourse[]" value="abcd"> abcd <br>
+        <input type="checkbox" id="" name="txtCourse[]" value="abcd"> abcd <br>
+        <input type="checkbox" id="" name="txtCourse[]" value="abcd"> abcd
       </div>
     </div>
     <div class="form-row pt-2">
@@ -92,7 +92,7 @@
       <div class="col-lg-5">
         <select class="text-field" id="txtTech" name="txtTech">
           <option value="">Choose........</option>
-          <option value="">Full Stack Web Development</option>
+          <option value="Full Stack Web Development">Full Stack Web Development</option>
           <option value="">Android</option>
           <option value="">Swift(IOS Developer)</option>
           <option value="">JAVA</option>
@@ -121,12 +121,14 @@
   </form>
 </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> --> -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 
-    <script type="text/javascript">
+    <!-- <script type="text/javascript">
     $(document).ready(function(){
       alert();
       $("#btnSubmit").click(function(e){
@@ -145,6 +147,19 @@
         var txtGender = $("#txtFemale").val();
         var txtDepartment = $("#txtDepartment").val();
         var txtCourse = $("#txtCourse").val();
+        if(empty($course))
+        {
+          echo "You didn't get select anything.";
+        }
+        else {
+          $N = count($course);
+
+          echo "selected";
+          for ($i=0; $i < $N ; $i++) {
+            // code...
+            echo ($course[$i] . " ");
+          }
+        }
         var txtPic = $("#txtPic").val();
         var txtTech = $("#txtTech").val();
         var txtHobbies = $("#txtHobbies").val();
@@ -162,6 +177,19 @@
       });
     });
 
+    function showpreview(file) {
+
+           if (file.files && file.files[0]) {
+               var reader = new FileReader();
+               reader.onload = function(ev) {
+                   $('#spic').attr('src', ev.target.result);
+               }
+               reader.readAsDataURL(file.files[0]);
+           }
+       }
+    </script> -->
+
+    <script type="text/javascript">
     function showpreview(file) {
 
            if (file.files && file.files[0]) {
