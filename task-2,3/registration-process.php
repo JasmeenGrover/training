084f11efbbd088451739c8405a29584fc8bc066f
Jameen Grover <jasmeengrover945@gmail.com>
2021-02-26 00:03:12 +0530

    Update registration-process.php

--- task-2,3/registration-process.php
+++ task-2,3/registration-process.php
@@ -3,25 +3,31 @@ include_once("connection.php");
 echo "<pre>";
 
 print_r($_POST);
-exit();
-$name=$_POST["name"];
-$studentid=$_POST["studentid"];
-$mobile=$_POST["mobile"];
-$email=$_POST["email"];
-$gender=$_POST["gender"];
-$department=$_POST["department"];
-$course=$_POST["course"];
-$pic=$_FILES["pic"]["name"];
-$tmpName=$_FILES["pic"]["tmp_Name"];
+// exit();
+$name=$_POST["txtName"];
+$studentid=$_POST["txtId"];
+$mobile=$_POST["txtMobile"];
+$email=$_POST["txtEmail"];
+$gender=$_POST["txtGender"];
+// var txtMale = $("#txtMale").val();
+// var txtFemale = $("#txtFemale").val();
+// if($("#txtMale").prop("checked") == true)
+// var txtGender = $("#txtMale").val();
+// if($("#txtFemale").prop("checked") == true)
+// var txtGender = $("#txtFemale").val();
+$department=$_POST["txtDepartment"];
+$course=$_POST["txtCourse"];
+$pic=$_FILES["txtPic"]["name"];
+$tmpName=$_FILES["txtPic"]["tmp_Name"];
 move_uploaded_file($tmpName,"upload/".$pic);
-$skills=$_POST["skills"];
-$hobbies=$_POST["hobbies"];
+$skills=$_POST["txtSkills"];
+$hobbies=$_POST["txtHobbies"];
 // print_r($_GET);
 $query="INSERT INTO registration('name','studentid','mobile','email','gender','department','course','pic','skills','hobbies') VALUES('$name','$studentid','$mobile','$email','$gender','$department','$course','$pic','$skills','$hobbies')";
 print_r($query);
 mysqli_query($dbcon,$query);
 
-$msg=mysqli_error($dbcon);
+echo $msg=mysqli_error($dbcon);
 
 if($msg==""){
   echo "Data Saved";
