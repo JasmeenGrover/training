2bee2f468ce249a6e6d43ba1547f66c79dd3dad5
Jameen Grover <jasmeengrover945@gmail.com>
2021-02-26 19:01:35 +0530

    Update registration-process.php

--- task-2,3/registration-process.php
+++ task-2,3/registration-process.php
@@ -23,8 +23,15 @@ move_uploaded_file($tmpName,"upload/".$pic);
 $skills=$_POST["txtTech"];
 $hobbies=$_POST["txtHobbies"];
 // print_r($_GET);
-$query="INSERT INTO registration('name','studentid','mobile','email','gender','department','course','pic','skills','hobbies') VALUES('$name','$studentid','$mobile','$email','$gender','$department','$course','$pic','$skills','$hobbies')";
+$query="INSERT INTO registration('name','studentid','mobile','email','gender','department','course','pic','skills','hobbies') VALUES ('$name','$studentid','$mobile','$email','$gender','$department','$course','$pic','$skills','$hobbies')";
+
 print_r($query);
+
+$query = array('name','studentid','mobile','email','gender','department','course','pic','skills','hobbies');
+$comma_separated = implode(",", $query);
+
+echo $comma_separated;
+
 mysqli_query($dbcon,$query);
 
 echo $msg=mysqli_error($dbcon);
