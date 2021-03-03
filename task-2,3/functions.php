179985211cd4395428d27657aca2de0db610fc29
Jameen Grover <jasmeengrover945@gmail.com>
2021-03-03 13:17:55 +0530

    Create functions.php

---
+++ task-2,3/functions.php
@@ -0,0 +1,27 @@
+<?php
+function doDelete($dbcon){
+  $name=$_POST["txtName"];
+  $studentid=$_POST["txtId"];
+  $mobile=$_POST["txtMobile"];
+  $email=$_POST["txtEmail"];
+  $gender=$_POST["txtGender"];
+  $department=$_POST["txtDepartment"];
+  $course=$_POST["txtCourse"];
+  $course = implode(",", $course);
+  echo $course;
+  $pic=$_FILES["txtPic"]["name"];
+  $tmpName=$_FILES["txtPic"]["tmp_Name"];
+  move_uploaded_file($tmpName,"upload/".$pic);
+  $skills=$_POST["txtTech"];
+  $hobbies=$_POST["txtHobbies"];
+
+  $query = "delete from registration where studentid = '$studentid'";
+
+
+  mysqli_query($dbcon,$query);
+  $count=mysqli_affected_rows($dbcon);
+  echo $count."Delete";
+}
+
+doDelete();
+ ?>
