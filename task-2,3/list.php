235d3a0c377bfcd1a00ad504f52163ecb44b423c
Jameen Grover <jasmeengrover945@gmail.com>
2021-03-05 21:31:24 +0530

    Update list.php

--- task-2,3/list.php
+++ task-2,3/list.php
@@ -39,9 +39,11 @@
           <td><?php echo $row["name"]; ?> </td>
           <td><?php echo $row["mobile"]; ?> </td>
           <td class="text-center">
-            <a class='btn btn-info btn-xs' href="edit.php?id=<?php  echo $row["studentid"]; ?>"><span class="glyphicon glyphicon-edit"></span> Edit</a> 
-            <a href="list-process.php?id=<?php  echo $row["studentid"]; ?>" name="btn" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Del</a>
-          </td>
+            <a class='btn btn-info btn-xs' href="registration.php?id=<?php  echo $row["studentid"]; ?>">
+              <span class="glyphicon glyphicon-edit"></span> Edit</a>
+              <a href="list-process.php" name="btn" class="btn btn-danger btn-xs">
+                <span class="glyphicon glyphicon-remove"></span> Del</a>
+              </td>
       </tr>
       <?php
       $i++;
@@ -58,11 +60,10 @@
      ?>
     </div>
 </div>
+  </body>
 
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-
-  </body>
 </html>
