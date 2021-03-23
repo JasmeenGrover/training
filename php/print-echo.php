ac7d7dc4fa1b4417b5c447be40e9d604260e6ee7
Jameen Grover <jasmeengrover945@gmail.com>
2021-03-23 23:58:54 +0530

    php

---
+++ php/print-echo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $var = "Hello";
+    echo $var;
+     ?>
+
+     <?php
+     $book = "The Merchant of Venice";
+     $author = "Shakespeare";
+
+     echo "<h2>".$book. " is written by " .$author."</h2>";
+      ?>
+  </body>
+</html>
