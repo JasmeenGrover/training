84b06850be9a21071ab74d4ff3413bf2df9e4de6
Jameen Grover <jasmeengrover945@gmail.com>
2021-03-25 23:08:29 +0530

    PHP

---
+++ php/constant.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    define("NOVEL", "The Merchant Of Venice"); //case-sensitive
+    echo NOVEL;
+     ?>
+     <!-- <?php
+     define("BOOK", "The Merchant Of Venice by Shakespeare", true); //case-insensitive doesn't work in php 7 above
+     echo book;
+      ?> -->
+      <!-- <?php
+      define ('cars', ['BMW',
+      'RollsRoyce',
+      'Ferrari'
+    ]);
+      echo cars[0];
+       ?> -->
+       
+  </body>
+</html>
---
+++ php/operators.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $a=10;
+    $b=20;
+    $c=$a+$b;// add operator
+    echo $c;
+    $c=$b-$a;// subtraction operator
+    echo $c;
+    $c=$a*$b;// multiplication operator
+    echo $c;
+    $c=$b/$a;// division operator
+    echo $c;
+    $c=$a/$b;// modulus operator
+    echo $c;
+    $c=$a**$b;// exponentional operator
+    echo $c;
+     ?>
+  </body>
+</html>
