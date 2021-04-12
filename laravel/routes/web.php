e1a73a3d0eee377674793d74dfc52876823dcb2c
Jameen Grover <jasmeengrover945@gmail.com>
2021-04-12 16:47:16 +0530

    Laravel 8

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//
+Route::get('/hello', function () {
     return view('welcome');
 });
 
-Route::get('/welcome', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return ['god' => 'god'];
 });
